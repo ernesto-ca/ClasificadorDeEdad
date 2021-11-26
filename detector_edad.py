48f57fc932838f5a7dff327af3e0db75a666462a
avilaca873 <avilagallegoscarlos.cbtis145@gmail.com>
2021-11-25 23:40:42 -0600

    Ultima Actualización
    Se acoplo el codigo con las correcciones necesarias

--- detector_edad.py
+++ detector_edad.py
@@ -1,128 +1,143 @@
 # -*- coding: utf-8 -*-
-#-------------------FUENTE--------------------#
-#-https://www.thepythoncode.com/article/predict-age-using-opencv
-#---------------------------------------------#
-# age_net.caffemodel: It is the pre-trained model weights for age detection. You can download it here.
-# deploy_age.prototxt: is the model architecture for the age detection model (a plain text file with a JSON-like structure containing all the neural network layer’s definitions). Get it here.
-# res10_300x300_ssd_iter_140000_fp16.caffemodel: The pre-trained model weights for face detection, download here.
-# deploy.prototxt.txt: This is the model architecture for the face detection model, download here.
-# DNN = red neuronal profunda
-# CNN = red neuronal convolucional ()
-# Import Libraries
-import cv2
+#-----------------FUENTE-----------------
+#https://github.com/techycs18/age-detection-python-opencv
+#https://www.thepythoncode.com/article/predict-age-using-opencv
+#-----------------------------------------------
+# import the necessary packages
+
 import numpy as np
+import argparse
+import time
+import cv2
+from funciones_extras import image_resize
 
-# Importando funciones adicionales
-from funciones_extras import  image_resize
+def detectarYPredecir(frame, faceNet, ageNet, minConf=0.5):
+    #minConf = confianza minima (estimacion)
+    # definimos la lista donde se hubicara el rango de las edades que predecira el detector
+    AGE_BUCKETS = ["(0-2)", "(4-6)", "(8-12)", "(15-20)", "(25-32)", "(38-43)", "(48-53)", "(60-100)"]
 
-# The model architecture
-# download from: https://drive.google.com/open?id=1kiusFljZc9QfcIYdU2s7xrtWHTraHwmW
-AGE_MODEL = 'deploy_age.prototxt'
+    # Lista de los resultados
+    results = []
+
+    # agarrar la dimensiones de la foto y construir a traves de esta una gota para el analisis
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(image=frame, scalefactor=1.0, size=(300, 300),
+                                 mean=(104.0, 177.0, 123.0))
+
+    # pasando la gota a través de faceNet
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
+
+    # comenzando ciclo sobre las detenciones encontradas
+    for i in range(0, detections.shape[2]):
+        # extraer la los valores de confianza de cada detecion
+        confidence = detections[0, 0, i, 2]
+
+        # obteniendo la detección con un valor de confianza valuado mayor que el  mínimo
+        if confidence > minConf:
+            # obteniendo el recuadro para esa detección
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # consiguiendo el roi
+            face = frame[startY:endY, startX:endX]
+
+            # asegurarando el roi con el tamaño suficiente
+            if face.shape[0] < 20 or face.shape[1] < 20:
+                continue
+
+            faceBlob = cv2.dnn.blobFromImage(image=face, scalefactor=1.0, size=(227, 227),
+                                             mean=(78.4263377603, 87.7689143744, 114.895847746), swapRB=False)
+
+            # Realizando la prediccion de la edad obteniendo el dato con mayor probabilidad
+            ageNet.setInput(faceBlob)
+            pred = ageNet.forward()
+            i = pred[0].argmax()  # retornando el índice del cubo con la mayor probabilidad obtenida
+            age = AGE_BUCKETS[i]
+            ageConfidence = pred[0][i]
+
+            # se construye un diccionario que cuenta con la ubicación del cuadro delimitador de la cara junto con la predicción de la edad, posteriormente se actualiza nuestra lista de resultados
+            d = {
+                "loc": (startX, startY, endX, endY),
+                "age": (age, ageConfidence)
+            }
+            results.append(d)
+
+    return results
+
+
+parser = argparse.ArgumentParser(description='Poner minimo de probabilidad.')
+parser.add_argument("-p", "--predecir", type=float, default=0.5, help="Minimo de probabilidad para las predicciones.")
+args = vars(parser.parse_args())
+
+
+
+# El modelo arquitectonico
+# descargado de: https://drive.google.com/open?id=1kiusFljZc9QfcIYdU2s7xrtWHTraHwmW
+AGE_PROTO = 'deploy_age.prototxt'
 # The model pre-trained weights
-# download from: https://drive.google.com/open?id=1kWv0AjxGSN0g31OeJa02eBGM0R_jcjIl
-AGE_PROTO = 'age_net.caffemodel'
-# Each Caffe Model impose the shape of the input image also image preprocessing is required like mean
-# substraction to eliminate the effect of illunination changes
-MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
-# Represent the 8 age classes of this CNN  probability layer
-AGE_INTERVALS = ['(0, 2)', '(4, 6)', '(8, 12)', '(15, 20)',
-                 '(25, 32)', '(38, 43)', '(48, 53)', '(60, 100)']
-# download from: https://raw.githubusercontent.com/opencv/opencv/master/samples/dnn/face_detector/deploy.prototxt
+# descargado de: https://drive.google.com/open?id=1kWv0AjxGSN0g31OeJa02eBGM0R_jcjIl
+AGE_MODEL = 'age_net.caffemodel'
+# descargado de: https://raw.githubusercontent.com/opencv/opencv/master/samples/dnn/face_detector/deploy.prototxt
 FACE_PROTO = "deploy.prototxt"
-# download from: https://raw.githubusercontent.com/opencv/opencv_3rdparty/dnn_samples_face_detector_20180205_fp16/res10_300x300_ssd_iter_140000_fp16.caffemodel
+# descargado de: https://raw.githubusercontent.com/opencv/opencv_3rdparty/dnn_samples_face_detector_20180205_fp16/res10_300x300_ssd_iter_140000_fp16.caffemodel
 FACE_MODEL = "res10_300x300_ssd_iter_140000_fp16.caffemodel"
-# Initialize frame size
-frame_width = 1280
-frame_height = 720
-# load face Caffe model
-face_net = cv2.dnn.readNetFromCaffe(FACE_PROTO, FACE_MODEL)
-# Load age prediction model
-age_net = cv2.dnn.readNetFromCaffe(AGE_MODEL, AGE_PROTO)
-
-def get_faces(frame, confidence_threshold=0.5):
-    """Returns the box coordinates of all detected faces"""
-    # convert the frame into a blob to be ready for NN input
-    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104, 177.0, 123.0))
-    # set the image as input to the NN
-    face_net.setInput(blob)
-    # perform inference and get predictions
-    output = np.squeeze(face_net.forward())
-    # initialize the result list
-    faces = []
-    # Loop over the faces detected
-    for i in range(output.shape[0]):
-        confidence = output[i, 2]
-        if confidence > confidence_threshold:
-            box = output[i, 3:7] * np.array([frame_width, frame_height, frame_width, frame_height])
-            # convert to integers
-            start_x, start_y, end_x, end_y = box.astype(np.int)
-            # widen the box a little
-            start_x, start_y, end_x, end_y = start_x - \
-                10, start_y - 10, end_x + 10, end_y + 10
-            start_x = 0 if start_x < 0 else start_x
-            start_y = 0 if start_y < 0 else start_y
-            end_x = 0 if end_x < 0 else end_x
-            end_y = 0 if end_y < 0 else end_y
-            # append to our list
-            faces.append((start_x, start_y, end_x, end_y))
-    return faces
-
-def display_img(title, img):
-    """Displays an image on screen and maintains the output until the user presses a key"""
-    # Display Image on screen
-    cv2.imshow(title, img)
-    # Mantain output until user presses a key
-    cv2.waitKey(0)
-    # Destroy windows when user presses a key
-    cv2.destroyAllWindows()
-    
 
 
 
-def predict_age(input_path: str):
-    """Predict the age of the faces showing in the image"""
-    # Read Input Image
-    img = cv2.imread(input_path)
-    # Take a copy of the initial image and resize it
-    frame = img.copy()
-    if frame.shape[1] > frame_width:
-        frame = image_resize(frame, width=frame_width)
-    faces = get_faces(frame)
-    for i, (start_x, start_y, end_x, end_y) in enumerate(faces):
-        face_img = frame[start_y: end_y, start_x: end_x]
-        # image --> Input image to preprocess before passing it through our dnn for classification.
-        blob = cv2.dnn.blobFromImage(
-            image=face_img, scalefactor=1.0, size=(227, 227), 
-            mean=MODEL_MEAN_VALUES, swapRB=False
-        )
-        # Predict Age
-        age_net.setInput(blob)
-        age_preds = age_net.forward()
-        print("="*30, f"Face {i+1} Prediction Probabilities", "="*30)
-        for i in range(age_preds[0].shape[0]):
-            print(f"{AGE_INTERVALS[i]}: {age_preds[0, i]*100:.2f}%")
-        i = age_preds[0].argmax()
-        age = AGE_INTERVALS[i]
-        age_confidence_score = age_preds[0][i]
-        # Draw the box
-        label = f"Age:{age} - {age_confidence_score*100:.2f}%"
-        print(label)
-        # get the position where to put the text
-        yPos = start_y - 15
-        while yPos < 15:
-            yPos += 15
-        # write the text into the frame
-        cv2.putText(frame, label, (start_x, yPos),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), thickness=2)
-        # draw the rectangle around the face
-        cv2.rectangle(frame, (start_x, start_y), (end_x, end_y), color=(255, 0, 0), thickness=2)
-    # Display processed image
-    display_img('Age Estimator', frame)
-    # save the image if you want
-    cv2.imwrite("predicted_age.jpg", frame)
-    
-if __name__ == '__main__':
-    # Parsing command line arguments entered by user
-    import sys
-    image_path = sys.argv[1]
-    predict_age(image_path)
+# Generando el cnn para el reconocimiento facial
+faceNet = cv2.dnn.readNetFromCaffe(FACE_PROTO, FACE_MODEL)
+
+
+# Genere el cnn para reconocimiento de edad
+ageNet = cv2.dnn.readNetFromCaffe(AGE_PROTO, AGE_MODEL)
+
+# Inicializando la transmisión de video
+print("Abriendo la camara web...")
+vs = cv2.VideoCapture(0)
+time.sleep(2.0)
+contadorF = 0
+colorRecuadro = (0, 0, 0)
+# aqui se recorre la función de detección de edad que creamos
+while True:
+    # se cambia el tamaño de cada imagen
+    _,frame = vs.read()
+    frame = image_resize(frame, width=400)
+
+    #llamando a la función de detección de edad para cada imagen
+    results = detectarYPredecir(frame, faceNet, ageNet, minConf=args['predecir'])
+
+
+    for r in results:
+        #dibujando el recuadro alrededor de la cara y muestra la edad que se predijo
+        text = "{}: {:2f}%".format(r["age"][0], r["age"][1]*100)
+        (startX, startY, endX, endY) = r["loc"]
+        y = startY - 10 if startY - 10 > 10 else startY + 10
+        cv2.rectangle(frame, (startX, startY), (endX, endY), colorRecuadro, 2)
+        cv2.putText(frame, text, (startX, y),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
+        
+        if colorRecuadro == (255, colorRecuadro[1], colorRecuadro[2]):
+            colorRecuadro = (255, colorRecuadro[1]+1, 0)
+        elif colorRecuadro == (255, 255, colorRecuadro[2]):
+            colorRecuadro =  (255,255, colorRecuadro[2]+1 )
+        elif colorRecuadro == (255, 255, 255):
+            colorRecuadro = (0, 0, 0)
+        else:
+            colorRecuadro = (colorRecuadro[0]+1, 0, 0)
+    # mostrando la imagen de salida
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    # si se presionó la tecla `q`, se terminara el ciclo
+    if key == ord("q"):
+        break
+    if key == ord("p"):
+        nombreImagen ='fotos/prediccion{0}.jpg'.format(contadorF)
+        print(nombreImagen)
+        cv2.imwrite(nombreImagen, frame)
+        contadorF += 1
+
+# destruimos lo que ya no se necesita
+cv2.destroyAllWindows()
+vs.release()
+print("Hasta luego!")
--- funciones_extras.py
+++ funciones_extras.py
@@ -2,27 +2,23 @@
 import cv2
 
 
-# from: https://stackoverflow.com/questions/44650888/resize-an-image-without-distortion-opencv
+# de: https://stackoverflow.com/questions/44650888/resize-an-image-without-distortion-opencv
 def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
-    # initialize the dimensions of the image to be resized and
-    # grab the image size
+    # Inicializando las dimensiones de la imagen a cambiar de tamaño ademas de tomar el tamaño de la imagen.
     dim = None
     (h, w) = image.shape[:2]
-    # if both the width and height are None, then return the
-    # original image
+    # si tanto el ancho y el alto De la imagen no tienen nada se retorna la imagen original
     if width is None and height is None:
         return image
-    # check to see if the width is None
+    # comprobamos si el ancho es nulo
     if width is None:
-        # calculate the ratio of the height and construct the
-        # dimensions
+        # calculando la relación de la altura para construir las dimensiones
         r = height / float(h)
         dim = (int(w * r), height)
-    # otherwise, the height is None
+    # si no se cumple la condición la altura es nula
     else:
-        # calculate the ratio of the width and construct the
-        # dimensions
+        # se calcula la relación del ancho para construir las dimensiones correspondientes
         r = width / float(w)
         dim = (width, int(h * r))
-    # resize the image
+    # se cambia el tamaño de la imagen
     return cv2.resize(image, dim, interpolation = inter)
